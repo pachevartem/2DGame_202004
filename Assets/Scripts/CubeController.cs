941ccdfe6a1e89e4a4f6b0c1cc97c7b8af5b0150
Artem <pachevartem@ya.ru>
2020-04-23 18:00:59 +0300

    add Courutine

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    private Coroutine _r;
+    private int timer = 99;
+    private void Start()
+    {
+        _r = StartCoroutine("StopWatch");
+    }
+
+    IEnumerator StopWatch()
+    {
+        for (; ; )
+        {
+            timer--;
+            Debug.Log(timer);
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StopCoroutine(_r);
+        }
+    }
+}
