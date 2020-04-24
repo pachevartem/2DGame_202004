94c7c7464e9442cb96d50bb46a1078763a4b294e
Artem <pachevartem@ya.ru>
2020-04-24 13:41:31 +0300

    UI

---
+++ Assets/Scripts/FAQ_4/CheckComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace FAQ
+{
+    public class CheckComponent: MonoBehaviour
+    {
+        private Rigidbody _rigidbody;
+        private BoxCollider _boxCollider;
+        
+        private void Awake()
+        {
+            if (GetComponent<BoxCollider>()== null)
+            {
+                Debug.LogError("На твоём объекте нет компонента "+ typeof(BoxCollider));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FAQ_4/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace FAQ
+{
+    public class GameController: MonoBehaviour
+    {
+        private List<BoxCollider> _cubes = new List<BoxCollider>();
+
+        private void Awake()
+        {
+            _cubes = FindObjectsOfType<BoxCollider>().ToList();
+            Debug.Log(_cubes.Count);
+            foreach (var boxCollider in _cubes)
+            {
+                boxCollider.transform.Translate(Vector3.up);
+            }
+        }
+    }
+}
